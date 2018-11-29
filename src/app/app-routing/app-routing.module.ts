3f2331a9f98b58a16ff8a48487ea66adda3702f9
Louis <louisdiatta0@gmail.com>
2018-11-29 12:56:49 +0000

    Revert "Revert "first commit on Luwee's deskto""
    This reverts commit 2d465476c147c3806be11ff31072c4b2cf040093.

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../auth/auth-guard.service';
 import { HomeComponent } from '../home/home.component';
+import { SceneComponent } from '../scene/scene.component';
 
 const routes: Routes = [{
   path: '',
@@ -12,6 +13,10 @@ const routes: Routes = [{
 }, {
   path: 'admin',
   loadChildren: 'app/admin/admin.module#AdminModule'
+},
+{
+  path: 'scene',
+  component: SceneComponent
 }];
 
 @NgModule({
@@ -21,4 +26,4 @@ const routes: Routes = [{
   declarations: []
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
